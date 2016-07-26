7c8c300cdfc811cc0314177ddda4c030e3276da2
ejrbalma <ejrbalma@gmail.com>
2016-07-26 14:31:54 +0100

    Tercera Version, se incluye una variable estática MILLISECONDS_PER_YEAR.

--- Person.java
+++ Person.java
@@ -4,6 +4,7 @@ public class Person {
 
     private final String name;
     private final Date birthday;
+    private static final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     public Person(String name, Date birthday) {
         this.name = name;
@@ -24,6 +25,6 @@ public class Person {
     }
 
     private int milliSecondsToYear(long milliseconds) {
-        return (int) (milliseconds / (1000 * 60 * 60 * 24 * 365.25));
+        return (int) (milliseconds /  MILLISECONDS_PER_YEAR);
     }
 }
